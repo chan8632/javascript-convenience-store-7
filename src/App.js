25d089dc66c83a80542bf1cc16e945c67cb3a87e
chan8632 <duscks8632@gmail.com>
2024-11-11 15:56:25 +0900

    feat(FileLoader): 제품과 프로모션 데이터를 로드하는 기능 추가
    - 마크다운 파일에서 제품 및 프로모션 데이터를 읽어오는 loadProducts와 loadPromotions 함수 구현
    - 데이터 라인을 파싱하는 setParseProducts 및 setParsePromotions 헬퍼 함수 추가
    - 제품 항목의 프로모션 필드를 형식화하는 ormatPromotion 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { loadProducts, loadPromotions } from "./FileLoader.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const products = await loadProducts();
+    const promotions = await loadPromotions();
+  }
 }
 
 export default App;
---
+++ src/FileLoader.js
@@ -0,0 +1,45 @@
+import fs from "fs/promises";
+export async function loadProducts() {
+  const data = await fs.readFile("public/products.md", "utf8");
+  const lines = data.trim().split("\n");
+  const parseProducts = setParseProducts(lines);
+  return parseProducts;
+}
+
+export async function loadPromotions() {
+  const data = await fs.readFile("public/promotions.md", "utf8");
+  const lines = data.trim().split("\n");
+  const parsedPromotions = setParsePromotions(lines);
+  return parsedPromotions;
+}
+function setParseProducts(data) {
+  return data.slice(1).map((line) => {
+    const [name, price, quantity, promotion] = line.split(",");
+
+    function formatPromotion(promotion) {
+      if (promotion.trim() === null) {
+        return "null";
+      }
+      return promotion.trim();
+    }
+
+    return {
+      name: name.trim(),
+      price: parseInt(price.trim(), 10),
+      quantity: parseInt(quantity.trim(), 10),
+      promotion: formatPromotion(promotion),
+    };
+  });
+}
+function setParsePromotions(data) {
+  return data.slice(1).map((line) => {
+    const [name, buy, get, start_date, end_date] = line.split(",");
+    return {
+      name: name.trim(),
+      buy: parseInt(buy.trim(), 10),
+      get: parseInt(get.trim(), 10),
+      start_date: new Date(start_date.trim()),
+      end_data: new Date(end_date.trim()),
+    };
+  });
+}
