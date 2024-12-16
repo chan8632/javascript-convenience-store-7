dbab5315dd9e35d28584d210b50330d4d753a169
chan8632 <duscks8632@gmail.com>
2024-12-16 21:32:12 +0900

    초기설정

--- src/App.js
+++ src/App.js
@@ -4,34 +4,8 @@ import OutputView from "./views/OutputView.js";
 import InputView from "./views/InputView.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  constructor() {
-    this.productManger = new ProductManager();
-  }
   async run() {
-    OutputView.printProducts(this.productManger.getAvailableProducts());
-
-    const receipt = new Receipt();
-
-    while (true) {
-      const input = await InputView.readProductSelection();
-      const [productName, quantity] = this.parseInput(input);
-
-      try {
-        const product = this.productManger.getProductByName(productName);
-        this.productManger.updateStock(productName, quantity);
-
-        receipt.addItem(productName, quantity, product.price);
-
-        OutputView.printReceipt(receipt);
-
-        const additionalPurchase = await InputView.readAdditionalPurchase();
-        if (additionalPurchase.toLowerCase() !== "y") break;
-
-        OutputView.printProducts(this.productManger.getAvailableProducts());
-      } catch (error) {
-        MissionUtils.Console.print(error.message);
-      }
-    }
+    re;
   }
 }
 
